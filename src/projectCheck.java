a6b76ebbf04219679bd0ff3cc82d3300d08ca369
zhangqiao <zhangqiao@njbandou.com>
2019-01-03 20:03:21 +0800

    该覆盖准则的神经元对需要满足以下三个条件： （1）、存在两个输入X1,X2，当输入分别为X1和X2时，前一层的神经元距离足够小。 距离足够小的判断标准如下： 假设当输入为X1时，该层神经元的权值集合为U(X1) 当输入为X2时，该层神经元的权值的集合为U（X2） 当||U(X1)-U(X2)||≤5时，称一层神经元的距离足够小。        （2）、当输入为X1，X2时，条件神经元的这一层所有神经元的符号均未改变。        （3）、当输入为X1,X2时，决策神经元的符号产生了改变。
    .该覆盖准则的公式为：
    Distance-sign coverage=满足以上三个条件的神经元对的个数/总神经元对的个数
    
     注意事项：
    	我们认为，当神经元权值大于0时，符号为正，小于等于0时，符号为负，所谓符号改变，就是正负性产生了改变。
    判断距离足够小的公式为L1范数距离公式。

--- src/projectCheck.java
+++ src/projectCheck.java
@@ -1,9 +1,9 @@
-import com.sun.org.apache.bcel.internal.generic.LNEG;
 import jxl.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.*;
 
 /**
  * Created by zhangqiao on 2018/12/27.
@@ -11,10 +11,11 @@ import java.text.DecimalFormat;
 public class projectCheck {
     public static void main(String[] args) throws IOException {
         //distinction("E://data.xls");
-        double number_two_answer = number_two();
-        System.out.println("K段神经元覆盖率为："+number_two_answer);//0.9
+        //double number_two_answer = number_two();
+        //System.out.println("K段神经元覆盖率为："+number_two_answer);//0.36533
         //traverse_data(0);
         //System.out.println(isIn(1.8696005,0.185164824,-4.6258817,4.6323595,50));
+        System.out.println(number_nine("E://data.xls",50,40,30));
     }
     public static void distinction(String url) throws IOException {
 
@@ -63,9 +64,6 @@ public class projectCheck {
                 result = df.format(num_result);
                 System.out.println("神经元覆盖率为"+result);
             }
-             /*第二题*/
-
-             /*第九题*/
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -94,11 +92,6 @@ public class projectCheck {
                         if(two_cell.getType() == CellType.NUMBER){
                             NumberCell numberCellOne = (NumberCell)two_cell;
                             n = numberCellOne.getValue();
-                            /*if(numberCellOne.getValue() < 0){
-                                 n = Math.abs(numberCellOne.getValue());//取绝对值
-                            }else {
-                                 n = numberCellOne.getValue();
-                            }*/
                             colSum -= n;
                             weighs[j] = n;
                             if(colSum < 0){
@@ -116,23 +109,20 @@ public class projectCheck {
                 //double k_n_coverage = sumResult/(50*columns);
                 //
                 String result = "";
-                float num_result =(float)sumResult/(50*columns);
+                double num_result = (double) sumResult/(50*columns);
 
-                DecimalFormat df = new DecimalFormat("0.0");
+                DecimalFormat df = new DecimalFormat("0.00000");
 
                 result = df.format(num_result);
-                System.out.println("神经元覆盖率为"+result);
-                //
                 return  Double.parseDouble(result);
             }
-             /*第二题*/
-             /*第九题*/
         } catch (Exception e) {
             e.printStackTrace();
         }
         return 0;
     }
 
+    //遍历data
     public static int traverse_data(int col,double average_n,double weights[]){
         //System.out.println(average_n);
         /*for (int i =0;i < weights.length;i++){
@@ -143,11 +133,12 @@ public class projectCheck {
         Sheet[] sheets;
         Workbook book;
         Cell one_cell;
+        Set<Double> set = new TreeSet<>();
         try {
             book = Workbook.getWorkbook(file);
             sheets = book.getSheets();
-            int count = 0;
-            double result[] = {0.0};
+            //int count = 0;
+            //double result[] = {0.0};
             for (Sheet sheet : sheets) {
                 int rows = sheet.getRows();//当前工作表行数
                 int columns = sheet.getColumns();//当前工作表列数
@@ -155,14 +146,13 @@ public class projectCheck {
                 if(col <= columns){
                     for(int i = 0; i < rows;i++){
                        // for(int j = 0; j < columns;j++){
-
-
                             one_cell = sheet.getCell(col,i);//列在前行在后
                             if(one_cell.getType() == CellType.NUMBER){
                                 NumberCell numberCellOne = (NumberCell)one_cell;
                                 double testResult = isIn(numberCellOne.getValue(),average_n,weights[1],weights[0],50);
-                                System.out.println("testResult"+testResult+"i是"+col);
-                                if(i == 0){
+                               // System.out.println("testResult"+testResult+"i是"+col);
+                                set.add(testResult);
+                                /*if(i == 0){
                                     result[0] = testResult;
                                 }else {
                                     boolean flag = true;
@@ -175,15 +165,16 @@ public class projectCheck {
                                         count ++;
                                         result[result.length-1] = testResult;
                                     }
-                                }
+                                }*/
                             }
                         //}
                     }
                 }else {
                     break;
                 }
-                System.out.println("count是"+count);
-                return count;
+                //System.out.println("count是"+count);
+                return set.size() ;
+                //return  count;
             }
         }catch (Exception e){
             e.printStackTrace();
@@ -209,5 +200,129 @@ public class projectCheck {
             }
         }
         return -1;
+    }
+
+
+    //第九题
+    //参数：工作表url  第一层神经元个数  第二层神经元个数  第三层神经元个数
+    public static double number_nine(String url,int floor1,int floor2,int floor3){
+        File file = new File(url);
+
+        Sheet[] sheets;
+        Workbook book;
+        Cell two_cell;
+        int count = 0;
+        Set set1_2 = new TreeSet();
+        Set set2_3 = new TreeSet();
+
+        try {
+            book = Workbook.getWorkbook(file);
+            sheets = book.getSheets();
+
+            List<List> line_all = new ArrayList();
+
+            for (Sheet sheet : sheets) {
+                int rows = sheet.getRows();//当前工作表行数
+                int columns = sheet.getColumns();//当前工作表列数
+                int j = 0;
+                for(int i = 0; i < rows; i++){
+                    List<Double> line = new ArrayList();
+                    double n2 = 0.0;
+
+                    for(int k = 0;k < columns;k++){
+                        two_cell = sheet.getCell(k,i);
+                        if(two_cell.getType() == CellType.NUMBER) {
+                            NumberCell numberCellTwo = (NumberCell) two_cell;
+                            n2 = numberCellTwo.getValue();
+                        }
+                        line.add(n2);
+                    }
+                    line_all.add(line);
+                }
+            }
+            for(int i = 0; i < line_all.size();i++){
+                List<Double> x2 = null;
+                List<Double> x1 = line_all.get(i);
+                for(int j = i+1; j < line_all.size();j++){
+                    if(j == i && i <line_all.size()-1){
+                        j++;
+                    }
+                    if(i == line_all.size()-1){
+                        if(j == line_all.size()-1){
+                            break;
+                        }
+                    }
+                   x2 = line_all.get(j);
+                    boolean flag1_12 = interval_cn_nochange(x1.subList(0,floor1-1),x2.subList(0,floor1-1));
+                    List<Integer> flag1_3 = decision_neuron_change(x1.subList(floor1,floor1+floor2-1),x2.subList(floor1,floor1+floor2-1));
+
+                    boolean flag2_12 = interval_cn_nochange(x1.subList(floor1,floor1+floor2-1),x2.subList(floor1,floor1+floor2-1));
+                    List<Integer> flag2_3 = decision_neuron_change(x1.subList(floor1+floor2,floor1+floor2+floor3-1),x2.subList(floor1+floor2,floor1+floor2+floor3-1));
+                    if(flag1_12){
+                        if(flag1_3.size() > 0){
+                            for (int n:flag1_3) {
+                                set1_2.add(n);
+                            }
+                        }
+                    }
+                    if(flag2_12){
+                        if(flag2_3.size() > 0){
+                            for (int m:flag2_3) {
+                                set2_3.add(m);
+                            }
+                        }
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        count = floor1*set1_2.size() + floor2*set2_3.size();
+        String result = "";
+        double num_result = (double) count/(floor1*floor2+floor2*floor3);
+
+        DecimalFormat df = new DecimalFormat("0.00000");
+
+        result = df.format(num_result);
+        return  Double.parseDouble(result);
+    }
+
+    //条件一：存在两个输入X1,X2，当输入分别为X1和X2时，前一层的神经元距离足够小。
+    public  static boolean interval_cn_nochange(List<Double> x1,List<Double> x2){
+        boolean flag = false;
+        double ab_sum = 0.0;
+
+        for(int i = 0; i < x1.size();i++){
+            double n1 = x1.get(i);
+            double n2 = x2.get(i);
+
+            double m1 = n1-n2;
+            //条件二：当输入为X1，X2时，条件神经元的这一层所有神经元的符号均未改变。
+            //若不满足条件二直接退出
+            if(((n1 > 0) && (n2 <= 0)) || ((n1 <= 0)&&(n2 > 0))){
+                return  false;
+            }
+            if(m1 < 0){
+                m1 = Math.abs(m1);
+            }
+            ab_sum += m1;
+        }
+        if(ab_sum <= 50){
+           // System.out.println("sum是"+ab_sum);
+            flag = true;
+        }
+        return  flag;
+    }
+    //条件三：当输入为X1,X2时，决策神经元的符号产生了改变。
+    public static List<Integer> decision_neuron_change(List<Double> x1,List<Double> x2){
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < x2.size();i++){
+            double n1 = x1.get(i);
+            double n2 = x2.get(i);
+            if(((n1 > 0) && (n2 <= 0)) || ((n1 <= 0)&&(n2 > 0))){
+               list.add(i);
+            }
+        }
+        return list;
     }
 }
